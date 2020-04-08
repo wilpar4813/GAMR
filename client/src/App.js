844e4e955bf76c77c3dc3943962627a2a1d5a1ba
gordyclint <55668552+gordyclint@users.noreply.github.com>
2020-04-07 20:18:21 -0400

    debugging in MERN application

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home.js";
 
 function App() {
-  // return (
-
-  // )
+  return (
+    <Home/>
+  )
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const mongoose = require("mongoose");
-const routes = require("./routes");
+// const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -14,12 +14,12 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Add routes, both API and view
-app.use(routes);
+// app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gamrdb");
 
 // Start the API server
 app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  console.log(`🌎  ==> API Server now listening on PORT http://localhost/${PORT}!!`);
 });
