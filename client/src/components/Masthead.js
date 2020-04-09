fa4dba6a485b5d9d4e31a9ec0470247fd6cd338b
travis-john <travis.wl.john@gmail.com>
2020-04-09 12:38:54 -0400

    adding masthead images and psds

--- client/src/components/Masthead.js
+++ client/src/components/Masthead.js
@@ -4,7 +4,15 @@ import Container from 'reactstrap';
 class Masthead extends Component {
   render(){
     return(
-      
+      <Container>
+        <div className='masthead'>
+          <div className='masthead-container'>
+            <figure className='masthead-figure' role='presentation'>
+              <picture className='masthead-picture'></picture>
+            </figure>
+          </div>
+        </div>
+      </Container>
     )
   }
 }
