4312f5200182c07b82023c22348b2d09755092b4
Josh Faison <Joshfaiso@yahoo.com>
2020-04-11 10:55:59 -0400

    committing for master merge

---
+++ client/src/components/Login.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {ReactSVG} from 'react-svg';
+import image from 'images/gamer.svg';
+
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+
+const Login = (props) => {
+  return (
+      <>
+    <Form style={{background: "gold", margin: "70px"}}>
+      <FormGroup >
+        <Label for="exampleEmail">Email</Label>
+        <Input type="email" name="email" id="exampleEmail" placeholder="Email" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="examplePassword">Password</Label>
+        <Input type="password" name="password" id="examplePassword" placeholder="Password" />
+      </FormGroup>
+      <Button className="btn btn-danger" style={{border: "black"}}>Submit</Button>
+      <a href="#" style={{color: "black"}}>Register</a>
+    </Form>
+     <ReactSVG src={image}  beforeInjection={svg => {svg.setAttribute('style', 'width: 100%')}} />
+     </>
+  );
+}
+
+export default Login;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Login from '../components/Login';
 
 function Home () {
     return (
         <div>
-            Hello
+            <Login />
         </div>
     );
 }
