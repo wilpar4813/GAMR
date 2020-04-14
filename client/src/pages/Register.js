3188d1ccb9dc50bbe96527abcfa70a5b85721ad9
Tripp's Repo <tripp.parham@me.com>
2020-04-14 19:48:09 -0400

    utils fix

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,45 +1,50 @@
-import React, { Component } from 'react';
-import Register from '../components/Register';
-import { compare } from 'bcryptjs';
-import api from '../utils/api';
+import React, { Component } from "react";
+import Register from "../components/Register";
+import { compare } from "bcryptjs";
+import api from "../utils/api";
 
 export default class RegisterPage extends Component {
-     state= {
+    state = {
         userName: "",
         firstName: "",
         lastName: "",
         email: "",
-        password: ""
-     }
+        password: "",
+    };
 
     handleOnChange = (event) => {
-        const {name, value} = event.target;
+        const { name, value } = event.target;
         this.setState({
-            [name]: value        
-        })
-    }
+            [name]: value,
+        });
+    };
 
-   handleSubmit = (event) => {
-       event.preventDefault();
-       const userData = {
-           name : this.state.firstName + " " + this.state.lastName,
-           email: this.state.email,
-           password: this.state.password,
-           userName: this.state.userName
-       }
-    api.createUser(userData).then(data => {
-        console.log("success");
-
-    })
-   }
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const userData = {
+            name: this.state.firstName + " " + this.state.lastName,
+            email: this.state.email,
+            password: this.state.password,
+            userName: this.state.userName,
+        };
+        api.createUser(userData).then((data) => {
+            console.log("success");
+        });
+    };
 
     render() {
         return (
             <div>
-                <Register email = {this.state.email} firstName = {this.state.firstName} lastName = {this.state.lastName} password = {this.state.password} userName = {this.state.userName} 
-                handleOnChange = {this.handleOnChange} handleSubmit = {this.handleSubmit}/>
+                <Register
+                    email={this.state.email}
+                    firstName={this.state.firstName}
+                    lastName={this.state.lastName}
+                    password={this.state.password}
+                    userName={this.state.userName}
+                    handleOnChange={this.handleOnChange}
+                    handleSubmit={this.handleSubmit}
+                />
             </div>
-        )
+        );
     }
-  
 }
