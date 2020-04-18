a722534f1e7f96e8c8a03199fd0ebe954ae0d80d
travis-john <travis.wl.john@gmail.com>
2020-04-18 12:28:52 -0400

    reworking nav icons and breakpoint visibility

--- client/src/components/MainNav.js
+++ client/src/components/MainNav.js
@@ -1,8 +1,9 @@
 import React, { useState} from 'react';
 import { Container, Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, Form, Input, Button} from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub }from '@fortawesome/free-brands-svg-icons'
-import {ReactSVG} from 'react-svg';
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { ReactSVG } from 'react-svg';
 import Logo from '../svg/logo.svg';
 import Profile from '../pages/Profile';
 
@@ -18,12 +19,16 @@ const MainNav = (props) => {
         <NavbarBrand href="/">
           <ReactSVG src={Logo}  beforeInjection={svg => {svg.setAttribute('style', 'width: 100%')}} />
         </NavbarBrand>
-        <Button  href='/Profile' color='dark'>Profile</Button>
+        <Nav clasName='mr-auto' navbar>
+          <NavItem className='d-sm-none d-md-block'>
+            <NavLink  href='https://github.com/gordyclint/gamr-database-react-app' ><FontAwesomeIcon className='fas' icon={faGithub} /></NavLink>
+          </NavItem>
+        </Nav>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="ml-auto" navbar>
             <NavItem>
-              <NavLink href="https://github.com/gordyclint/gamr-database-react-app"><FontAwesomeIcon className='fab' icon={faGithub} /></NavLink>
+              <NavLink className='d-sm-inline-block' href="/profile"><FontAwesomeIcon className='fab' icon={faUserCircle} /></NavLink> <NavLink className='d-sm-inline-block d-md-none'  href='https://github.com/gordyclint/gamr-database-react-app' ><FontAwesomeIcon className='fas' icon={faGithub} /></NavLink>
             </NavItem>
             <UncontrolledDropdown nav inNavbar>
               <DropdownToggle nav caret>
