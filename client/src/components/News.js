fad303e751229a6904b39ffd79b2467c5fa40d1b
travis-john <travis.wl.john@gmail.com>
2020-04-20 14:06:45 -0400

    irregular layout rendering

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -9,6 +9,7 @@ const News = (props) => {
   async function fetchNews(){
     const res = await api.recentNews();
 
+    console.log(res);
     setNews(res.data);
   }
 
@@ -22,26 +23,26 @@ const News = (props) => {
       <Row>
       <h2 className='border-bottom-white text-white mb-3'>News</h2>
       </Row>
-        <Row>
-          {news.slice(0, 3).map(
-            (story) => (
-              <Col className='mb-2' sm='12' md='4'>
-                <Card inverse>
-                  <CardImg width="100%" src={story.image} alt="Card image cap" />
-                  <CardImgOverlay>
-                    <CardTitle>{story.title}</CardTitle>
-                    <CardText className='news-text'>{story.summary}</CardText>
-                    <CardText>
-                      <small className="text-muted"><a href='#'>{story.author}</a></small>
-                    </CardText>
-                  </CardImgOverlay>
-                  </Card>
-              </Col>
-            )
-          )}
+      <Row>
+        {news.slice(0, 3).map(
+          (story) => (
+            <Col className='mb-2' sm='12' md='4'>
+              <Card inverse>
+                <CardImg width="100%" src={story.image} alt="Card image cap" />
+                <CardImgOverlay>
+                  <CardTitle>{story.title}</CardTitle>
+                  <CardText className='news-text'>{story.summary}</CardText>
+                  <CardText>
+                    <small className="text-muted"><a href='#'>{story.author}</a></small>
+                  </CardText>
+                </CardImgOverlay>
+              </Card>
+            </Col>
+          )
+        )}
         </Row>
         <Row>
-          {news.slice(4).map(
+          {news.slice(3, 4).map(
             (story) => (
               <Col className='mb-2' sm='12' md='8'>
                 <Card inverse>
@@ -59,9 +60,9 @@ const News = (props) => {
             )}
           <Col className='mb-2' sm='12' md='4'>
             <Row>
-            {news.slice(5, 7).map(
+            {news.slice(4, 6).map(
               (story) => (
-                <Col className='mb-2' sm='12' md='4'>
+                <Col className='mb-2' sm='12'>
                   <Card inverse>
                     <CardImg width="100%" src={story.image} alt="Card image cap" />
                     <CardImgOverlay>
@@ -78,26 +79,29 @@ const News = (props) => {
           </Row>
         </Col>
       </Row>
-      <Row>
-      {news.slice(7, 9).map(
-        (story) => (
-          <Col className='mb-2' sm='12' md='6'>
-            <Card inverse>
-              <CardImg width="100%" src={story.image} alt="Card image cap" />
-              <CardImgOverlay>
-                <CardTitle>{story.title}</CardTitle>
-                <CardText className='news-text'>{story.summary}</CardText>
-                <CardText>
-                  <small className="text-muted"><a href='#'>{story.author}</a></small>
-                </CardText>
-              </CardImgOverlay>
-              </Card>
-          </Col>
-        )
-      )}
-    </Row>
-  </Container>
-  </div>
+      </Container>
+    </div>
+
+    //   <Row>
+    //   {news.slice(6, 9).map(
+    //     (story) => (
+    //       <Col className='mb-2' sm='12' md='6'>
+    //         <Card inverse>
+    //           <CardImg width="100%" src={story.image} alt="Card image cap" />
+    //           <CardImgOverlay>
+    //             <CardTitle>{story.title}</CardTitle>
+    //             <CardText className='news-text'>{story.summary}</CardText>
+    //             <CardText>
+    //               <small className="text-muted"><a href='#'>{story.author}</a></small>
+    //             </CardText>
+    //           </CardImgOverlay>
+    //           </Card>
+    //       </Col>
+    //     )
+    //   )}
+    // </Row>
+  // </Container>
+  // </div>
   )
 
 }
