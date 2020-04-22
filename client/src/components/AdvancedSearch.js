7a3b3088263e6cdc032a70dbbd74bc3e558b9764
travis-john <travis.wl.john@gmail.com>
2020-04-22 13:06:46 -0400

    adding SearchResult component

---
+++ client/src/components/AdvancedSearch.js
@@ -0,0 +1,2 @@
+import React from 'react';
+import {Container, Row, Col, Button, Form, FormGroup, Label, Input, FormText} from 'reactstrap';
---
+++ client/src/components/SearchResult.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Container, Row, Col, Media} from 'reactstrap';
+
+const SearchResult = (props) => {
+
+  return(
+
+  )
+}
+
+export default SearchResult;
