b94c124dc358b4a11fb9e7577749054e597e4961
travis-john <travis.wl.john@gmail.com>
2020-04-21 21:12:54 -0400

    adding GameList component

--- client/src/components/Popular.js
+++ client/src/components/Popular.js
@@ -1,13 +1,12 @@
 import React, {useState, useEffect } from 'react';
-import { Container, Row, Col, Button, ListGroup, ListGroupItem, Card, CardTitle, CardText, CardImg, CardImgOverlay, CardFooter } from 'reactstrap';
+import { Container, Row, Col, Button, ListGroup, ListGroupItem} from 'reactstrap';
+import GameList from './GameList'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faChevronDown, faGamepad} from '@fortawesome/free-solid-svg-icons';
 import { faPlaystation, faXbox  }from '@fortawesome/free-brands-svg-icons';
 import {ReactSVG} from 'react-svg';
 import NintendoSwitch from '../svg/nintendo-switch.svg';
-import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
-import ProgressProvider from '../utils/ProgressProvider';
-import 'react-circular-progressbar/dist/styles.css';
+
 import api from '../utils/api';
 
 
@@ -75,30 +74,7 @@ const Popular = (props) => {
         </Col>
         <Col sm='12' md='8'>
           <Row>
-          {allPopular.slice(0, showItems).map(
-            (game) => (
-              <Col className='mb-3' sm='12' md='6'>
-                <Card>
-                  <CardImg width="100%" src={game.cover.url} alt="Card image cap" />
-                  <CardImgOverlay>
-                    <div className='rating'>
-                      <ProgressProvider valueStart={0} valueEnd={Math.round(game.rating)}>
-                        {(value) => <CircularProgressbarWithChildren value={Math.round(game.rating)}>
-                          <div className='w-100 h-100 d-flex justify-content-center align-items-center'>
-                            <p className='CircularProgressbar-text mt-2'>{Math.round(game.rating)}</p>
-                          </div>
-                        </CircularProgressbarWithChildren>}
-                      </ProgressProvider>
-                    </div>;
-                  </CardImgOverlay>
-                  <CardFooter>
-                    <CardTitle>{game.name}</CardTitle>
-                    <CardText><small>franchise</small></CardText>
-                  </CardFooter>
-                </Card>
-              </Col>
-            )
-          )}
+          <GameList />
             <Button  onClick={updateShowItems} className='mx-auto update-trigger' color='link'>Load More <FontAwesomeIcon className='fas' icon={faChevronDown} /></Button>
           </Row>
         </Col>
