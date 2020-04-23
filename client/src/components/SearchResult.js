9bdb98e435f6d8dd9789a8def400f1c699c11659
travis-john <travis.wl.john@gmail.com>
2020-04-23 09:26:10 -0400

    search result styling

--- client/src/components/SearchResult.js
+++ client/src/components/SearchResult.js
@@ -8,27 +8,19 @@ const SearchResult = (props) => {
 
   return props.results.map(
     (result) => (
-      <Col className='mb-3' sm='12' md='6'>
-        <Card>
-          <CardImg src={result.cover.url} alt="Card image cap" />
-          <CardImgOverlay>
-            <div className='rating'>
-              <ProgressProvider valueStart={0} valueEnd={Math.round(result.rating)}>
-                {(value) =>
-                  <CircularProgressbarWithChildren value={Math.round(result.rating)}>
-                    <div className='w-100 h-100 d-flex justify-content-center align-items-center'>
-                      <p className='CircularProgressbar-text mt-2'>{Math.round(result.rating)}</p>
-                    </div>
-                  </CircularProgressbarWithChildren>}
-              </ProgressProvider>
-            </div>
-          </CardImgOverlay>
-          <CardFooter>
-            <CardTitle>{result.name}</CardTitle>
-            <CardText><small>franchise</small></CardText>
-          </CardFooter>
-        </Card>
-      </Col>
+      <Col className='mb-3' sm='12'>
+        <Media className='border-bottom'>
+          <Media left href={`/games/${result.id}`}>
+            <Media object data-src={result.cover.url} alt={`${result.name} cover image`} />
+          </Media>
+          <Media body>
+            <Media heading>
+              {result.name}
+            </Media>
+          {result.summary}
+          </Media>
+        </Media>
+    </Col>
     )
   )
 }
