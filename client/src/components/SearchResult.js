62595bf127d02a80292618e2bfba00fa04e3ea4b
travis-john <travis.wl.john@gmail.com>
2020-04-23 09:50:36 -0400

    adding padding

--- client/src/components/SearchResult.js
+++ client/src/components/SearchResult.js
@@ -6,9 +6,9 @@ const SearchResult = (props) => {
   return props.results.map(
     (result) => (
       <Col className='mb-3' sm='12'>
-        <Media className='border-bottom'>
+        <Media className='border-bottom py-2'>
           <Media left className='mr-3' href={`/games/${result.id}`}>
-            <Media object src={result.cover.url} alt={`${result.name} cover image`} />
+            <Media object src={result.cover.url} />
           </Media>
           <Media body>
             <Media heading>
