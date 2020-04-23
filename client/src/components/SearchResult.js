60d4dd86d3f887563eb00f44d5d6096e65b84069
travis-john <travis.wl.john@gmail.com>
2020-04-23 15:12:54 -0400

    committing for class

--- client/src/components/SearchResult.js
+++ client/src/components/SearchResult.js
@@ -8,14 +8,14 @@ const SearchResult = (props) => {
       <Col className='mb-3' sm='12'>
         <Media className='border-bottom py-2'>
           <Media left>
-            <Media className='mr-2' object src={result.cover.url} alt="game title cover art here" />
+            <Media className='mr-2' object src={result.cover.url} alt={`${result.name} cover image`} />
           </Media>
           <Media body>
             <Media heading><a href={`/games/${result.id}`}>{result.name}</a></Media>
               {result.summary}
               <Media body className='d-flex justify-content-between align-items-center'>
-                <div><p>Release Date: <span className='release-date'>{result.first_release_date}</span></p></div>
-                <div><p>Time to beat: <span className='beat-time'>{result.time_to_beat.normally}</span></p></div>
+                <div><p>Release Date: <span className='release-date'>{result.release_dates.human}</span></p></div>
+                <div><p>Platform: <span className='platform'>{result.release_dates.platform}</span></p></div>
                 <div><p>Franchise: <span className='franchise'>{result.franchise.name}</span></p></div>
               </Media>
             </Media>
