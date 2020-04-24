cfe46a703844c119465ee415645705abe6e15558
gordyclint <55668552+gordyclint@users.noreply.github.com>
2020-04-24 18:56:20 -0400

    adding id based search functionality for api

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -53,9 +53,9 @@ class Home extends Component {
       <>
       { this.state.username != undefined ? console.log('LOOK HERE', this.state.username) : ''}
         <Masthead />
-        <Popular />
+        {/* <Popular />
         <News />
-        <Upcoming />
+        <Upcoming /> */}
         <Footer />
         </>
     );
