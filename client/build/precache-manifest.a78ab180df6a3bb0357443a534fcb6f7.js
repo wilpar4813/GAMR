8d362d33fa6b89a4e9b239f592eacb6bc58bb605
Tripp's Repo <tripp.parham@me.com>
2020-07-08 14:59:59 -0400

    add build

---
+++ client/build/precache-manifest.a78ab180df6a3bb0357443a534fcb6f7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4d3f5320faffad61871",
+    "url": "/static/css/main.38df03a1.chunk.css"
+  },
+  {
+    "revision": "a4d3f5320faffad61871",
+    "url": "/static/js/main.a28cc920.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dde166347cc030339959",
+    "url": "/static/css/2.6def8fa4.chunk.css"
+  },
+  {
+    "revision": "dde166347cc030339959",
+    "url": "/static/js/2.59d8cbaa.chunk.js"
+  },
+  {
+    "revision": "9aa86c20cb01f37dcbdc3e1b9119e603",
+    "url": "/static/media/logo.9aa86c20.svg"
+  },
+  {
+    "revision": "c540dd76e364a282e69c3533d15715bd",
+    "url": "/static/media/nintendo-switch.c540dd76.svg"
+  },
+  {
+    "revision": "985490b48edb348c376e15ef086f476d",
+    "url": "/static/media/gamer.985490b4.svg"
+  },
+  {
+    "revision": "fc63d1495acc37a8d6e4db2350762350",
+    "url": "/static/media/gallery-bg.fc63d149.svg"
+  },
+  {
+    "revision": "3fbfad4a383a7a9a0e5b898d6a8bbcfd",
+    "url": "/index.html"
+  }
+];
